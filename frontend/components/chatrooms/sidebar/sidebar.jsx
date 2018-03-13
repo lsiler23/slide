3f3ba5109f7cf98f737b0d0d4c29a5a102ee54d0
Laurette Siler <siler.laurette@gmail.com>
2018-03-13 13:39:23 -0400

    Final fixes before bonus

--- frontend/components/chatrooms/sidebar/sidebar.jsx
+++ frontend/components/chatrooms/sidebar/sidebar.jsx
@@ -12,10 +12,6 @@ export default class SideBar extends React.Component {
     };
   }
 
-  componentWillReceiveProps(newProps) {
-    
-  }
-
   handleLogout(e) {
     this.props.logout().then(() => this.props.history.push('/login'));
   }
